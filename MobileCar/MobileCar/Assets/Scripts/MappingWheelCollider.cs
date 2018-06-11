854b7fd13818391c35b01c0f355211b090d64fbd
NerbzHub <nathan.nette@gmail.com>
2018-06-11 23:54:14 +1000

    Up to https://www.youtube.com/watch?v=QFqJpQ99l_4 Which is number 6 in the series.

---
+++ MobileCar/MobileCar/Assets/Scripts/MappingWheelCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MappingWheelCollider : MonoBehaviour
+{
+
+    public WheelCollider wc;
+
+    private Vector3 wcCentre;
+    private RaycastHit hit;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wcCentre = wc.transform.TransformPoint(wc.center);
+
+        if (Physics.Raycast(wcCentre, -wc.transform.up, out hit, wc.suspensionDistance + wc.radius))
+        {
+            transform.position = hit.point + (wc.transform.up * wc.radius);
+        }
+        else
+        {
+            transform.position = wcCentre - (wc.transform.up * wc.suspensionDistance);
+        }
+    }
+}
